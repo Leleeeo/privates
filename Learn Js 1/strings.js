afeb9d7a9bfb616de058cb6a496e8acdd07a51fc
Leonardo Vieira <133938663+Leleeeo@users.noreply.github.com>
2023-12-27 00:21:27 -0300

    Add files via upload

---
+++ Learn Js 1/strings.js
@@ -0,0 +1,21 @@
+// string
+
+let leticia= "roi-leticia-ne";
+
+// concatenar strings
+
+let msg1 = "Roi";
+let msg2 = " Leticia né?";
+
+let concatenar = msg1 + msg2;
+
+let pessoa ="Leticia";
+let msg3 = `${pessoa}, ta tudo bem?`;
+
+// exercicio
+
+let usuario = prompt("What is your name?")
+alert(`${usuario}, welcome!`)
+console.log(usuario)
+
+
